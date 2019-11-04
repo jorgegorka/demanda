02fb1b63cfa926ad47eb51eb8282ec50bbda1788
Jorge Alvarez <jorge@alvareznavarro.es>
2019-11-04 10:24:30 +0100

    Update gems.

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -12,8 +12,8 @@ require "action_mailbox/engine"
 # require "action_text/engine"
 # require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
