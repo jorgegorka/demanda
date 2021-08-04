a98f7ac189d435b87b6fc90ba2b9a11540d90d1d
Jorge Alvarez <jorge@alvareznavarro.es>
2021-08-04 19:12:41 +0200

    Fix test.

--- backend/spec/models/cart_spec.rb
+++ backend/spec/models/cart_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Cart, type: :model do
   it_behaves_like 'uuidable'
 
   it { is_expected.to belong_to :account }
-  it { is_expected.to belong_to :user }
+  it { is_expected.to belong_to(:user).optional }
 end
